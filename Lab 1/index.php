0a52e3c748bd0448919cc0e9b4e1474d95f3823d
sunDalik <dali1999@mail.ru>
2018-08-30 19:50:15 +0300

    new file for styles

--- Lab 1/index.php
+++ Lab 1/index.php
@@ -2,70 +2,8 @@
 <head> 
 	<title>Лабораторная работа</title>
 	<link rel="icon" href= "images/COTN.png">
+	<link rel="stylesheet" href="styles.css">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<style type="text/css">
-	header{
-		margin-bottom: 120px;
-	}
-	body{
-		background: url("images/BG.png") no-repeat center center fixed;
-		background-size: cover; 
-	}
-	table{
-		background-color: #ffffff;
-		border-collapse: collapse;
-	}
-	td{
-		padding: 4px;
-		border: 1px solid #000000;
-	}
-	.box{
-		background-color: #e8e8e8;
-		border-radius: 15px;
-		display: inline-block;
-		padding: 10px;
-		line-height: 35px;
-		font-size: 18px;
-	}
-	.boxHeader{
-		line-height: 20px;
-		font-size: 16px;
-	}
-	.content{
-
-	}
-	.boxError, .boxResult{
-		line-height: 30px;
-	}
-	.boxError{
-		background-color: #ff7f7f;
-	}
-	.boxResult{
-		background-color: #97ff89; 
-	}
-	.hidden{
-		display: none;
-	}
-	.formContainer, .flexCenter{
-		display: flex;
-		align-items: center;
-		justify-content: center;
-	}
-
-	@media (max-width: 550px) {
-		.formContainer{
-			flex-direction: column;
-		}
-		header{
-			margin-bottom: 70px;
-		}
-	}
-	@media (min-width: 551px){
-		.boxSpaceRight{
-			margin-right: 50px;
-		}
-	}
-</style>
 <script type="text/javascript" src="something.js"></script>
 </head>
 
