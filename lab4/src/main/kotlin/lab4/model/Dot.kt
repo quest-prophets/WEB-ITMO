3f6dcce477f06f8dc8bee8233d9a8e3ae68ee922
nailstorms <nailstorm@protonmail.ch>
2019-02-01 17:05:39 +0300

    backend: init

---
+++ lab4/src/main/kotlin/lab4/model/Dot.kt
@@ -0,0 +1,7 @@
+package lab4.model
+
+class Dot(var x: Double, var y: Double, var hit: Boolean) {
+
+    override fun toString() = "X: $x Y: $y HIT: $hit"
+
+}
---
+++ lab4/src/main/kotlin/lab4/model/Result.kt
@@ -0,0 +1,20 @@
+package lab4.model
+
+import javax.persistence.*
+
+@Entity
+@Table(name = "hit_result")
+data class Result(var x: Double = 0.0,
+                         var y: Double = 0.0,
+                         var r: Double = 0.0,
+                         var isHit: Boolean = false) {
+
+    @Id
+    @GeneratedValue
+    var id: Long? = null
+
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "user_id", referencedColumnName = "id", nullable = false)
+    var user: UserInfo? = null
+
+}
