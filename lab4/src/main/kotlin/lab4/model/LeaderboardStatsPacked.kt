125c6da611a404ad3e8d748bce83a960e4916afc
nailstorms <nailstorm@protonmail.ch>
2019-03-10 21:27:51 +0300

    s

--- lab4/src/main/kotlin/lab4/model/LeaderboardStatsPacked.kt
+++ lab4/src/main/kotlin/lab4/model/LeaderboardStatsPacked.kt
@@ -3,4 +3,4 @@ package lab4.model
 data class LeaderboardStatsPacked (var place: Int?, var name: String = "",
                                    var winCount: Long?, var gameCount: Long?,
                                    var dotCount: Long?, var overallTime: Long?,
-                                   var performance: Double?)
+                                   var score: Long?, var performance: Double?)
